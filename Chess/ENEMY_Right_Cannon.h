84249a12213ba95df06680bd7810e42c58a0c4a8
15982683979 <15982683979@163.com>
2018-10-13 23:35:02 +0800

    _0

--- Chess/ENEMY_Right_Cannon.h
+++ Chess/ENEMY_Right_Cannon.h
@@ -78,31 +78,77 @@ public:
 				for (int j = 0; j < 11; j++)
 				{
 					bool IfOkMove = false;
-					if (Piece_Logic::mMouseDownRange[i][j] == 110)//索引到棋子右边的炮
+					if (Piece_Logic::mMouseDownRange[i][j] == 110)//索引到棋子左边的炮
 					{
 						int Temp_X = ENEMY_Command::__mReturnRedCurStandardCoor__().x;
 						int Temp_Y = ENEMY_Command::__mReturnRedCurStandardCoor__().y;//得到鼠标点击的标准坐标
-
 						int Move_Y = (mENEMY_Right_Cannon_Y - Temp_Y) / 60;//得到要移动的次数，同时也是目标的Y坐标
-
+						int Index = 0;//通过这个索引来执行吃棋
+						int Index_I = 0;//记录索引到的第一个棋
+						int Index_J = 0;//记录索引到的第一个棋
+						int Index_I_J = 0;//记录索引到的第一个棋的次数
+						int Index_N = 0;//记录从索引到第一个棋的又开始索引的棋子个数是否违反规则
+						int Index_N_I = 0;//记录索引了几次才索引到被攻击的棋子
+						int Index_Frequency = 0;//保证只执行一次
 						for (int k = 0; k < Move_Y; k++)//进行巡路是否可以行走，可以走多少
 						{
+							Index_I_J++;//记录索引到的第一个棋的次数
+							if ((Piece_Logic::mMouseDownRange[i - (k + 1)][j] < 200 &&
+								Piece_Logic::mMouseDownRange[i - (k + 1)][j] != 999))
+							{
+								if (Index_Frequency == 0)//保证只执行一次
+								{
+									Index++;//只要是棋子,不管敌方还是我方，都能作为桥梁
+									Index_I = (i - (k + 1));//记录索引到的第一个桥梁
+									Index_J = j;//记录索引到的第一个桥梁
+									if (Index == 1)//通过这个桥梁找敌方棋子
+									{
+										for (int n = 0; n <= Move_Y - Index_I_J - 1; n++)//通过剩下的所有的索引来找这个桥梁找敌方被吃掉棋子的次数
+										{
+											Index_N_I++;
+											if ((Piece_Logic::mMouseDownRange[Index_I - (n + 1)][Index_J] <= 17 &&
+												Piece_Logic::mMouseDownRange[Index_I - (n + 1)][Index_J] < 200 &&
+												Piece_Logic::mMouseDownRange[Index_I - (n + 1)][Index_J] != 999))
+											{
+												Index_N++;//如果只有一个则可以吃了，如果>1 则违反了规则
+											}
+											if (Index_N == 1 && n == Move_Y - Index_I_J - 1)//保证只有一个棋并且是你点击的地方
+											{
+												Index = 2;
+												Index_Frequency = 1;//保证一次索引到位
+												break;
+											}
+										}
+									}
+								}
+							}
+							if (Index == 2)//炮找到了攻击的对象
+							{
+								Piece_Logic::mMouseDownRange[i - Move_Y][Index_J] = Piece_Logic::mMouseDownRange[i][j];
+								Piece_Logic::mMouseDownRange[i][j] = 258;
+								mENEMY_Right_Cannon_Y -= (Move_Y * 60);
+
+								Piece_Logic::__WE__mWE_CommandState(0);//这里重要
+								Piece::Change_mWe(0);
+								ifok = true;
+								break;
+							}
+							else if (Index > 2)
+							{
+								break;
+							}
+							//==========================普通移动
 							if (Piece_Logic::mMouseDownRange[i - (k + 1)][j] >= 200 && Piece_Logic::mMouseDownRange[i - (k + 1)][j] != 999)
 							{
-								IfOkMove = true;
+								if (k == Move_Y - 1)//证明这条路可以走得通
+								{
+									IfOkMove = true;
+								}
 							}
-							else
+							else//不是吃棋或者移动，那还执行个鸡巴
 							{
 								IfOkMove = false;
 								break;
-							}
-							if (IfOkMove == false) //只要是一次false就是不行的
-							{
-								break;
-							}
-							if (true)
-							{
-								//这里可以判断炮可以吃棋了
 							}
 							if (IfOkMove && k == Move_Y - 1)//证明这条路可以走得通
 							{
@@ -131,31 +177,77 @@ public:
 				for (int j = 0; j < 11; j++)
 				{
 					bool IfOkMove = false;
-					if (Piece_Logic::mMouseDownRange[i][j] == 110)//索引到棋子右边的炮
+					if (Piece_Logic::mMouseDownRange[i][j] == 110)//索引到棋子左边的炮
 					{
 						int Temp_X = ENEMY_Command::__mReturnRedCurStandardCoor__().x;
 						int Temp_Y = ENEMY_Command::__mReturnRedCurStandardCoor__().y;//得到鼠标点击的标准坐标
-
 						int Move_Y = (Temp_Y - mENEMY_Right_Cannon_Y) / 60;//得到要移动的次数，同时也是目标的Y坐标
-
+						int Index = 0;//通过这个索引来执行吃棋
+						int Index_I = 0;//记录索引到的第一个棋
+						int Index_J = 0;//记录索引到的第一个棋
+						int Index_I_J = 0;//记录索引到的第一个棋的次数
+						int Index_N = 0;//记录从索引到第一个棋的又开始索引的棋子个数是否违反规则
+						int Index_N_I = 0;//记录索引了几次才索引到被攻击的棋子
+						int Index_Frequency = 0;//保证只执行一次
 						for (int k = 0; k < Move_Y; k++)//进行巡路是否可以行走，可以走多少
 						{
+							Index_I_J++;//记录索引到的第一个棋的次数
+							if ((Piece_Logic::mMouseDownRange[i + (k + 1)][j] < 200 &&
+								Piece_Logic::mMouseDownRange[i + (k + 1)][j] != 999))
+							{
+								if (Index_Frequency == 0)//保证只执行一次
+								{
+									Index++;//只要是棋子,不管敌方还是我方，都能作为桥梁
+									Index_I = (i + (k + 1));//记录索引到的第一个桥梁
+									Index_J = j;//记录索引到的第一个桥梁
+									if (Index == 1)//通过这个桥梁找敌方棋子
+									{
+										for (int n = 0; n <= Move_Y - Index_I_J - 1; n++)//通过剩下的所有的索引来找这个桥梁找敌方被吃掉棋子的次数
+										{
+											Index_N_I++;
+											if ((Piece_Logic::mMouseDownRange[Index_I + (n + 1)][Index_J] <= 17
+												&& Piece_Logic::mMouseDownRange[Index_I + (n + 1)][Index_J] < 200
+												&& Piece_Logic::mMouseDownRange[Index_I + (n + 1)][Index_J] != 999))
+											{
+												Index_N++;//如果只有一个则可以吃了，如果>1 则违反了规则
+											}
+											if (Index_N == 1 && n == Move_Y - Index_I_J - 1)//保证只有一个棋并且是你点击的地方
+											{
+												Index = 2;
+												Index_Frequency = 1;//保证一次索引到位
+												break;
+											}
+										}
+									}
+								}
+							}
+							if (Index == 2)//炮找到了攻击的对象
+							{
+								Piece_Logic::mMouseDownRange[i + Move_Y][Index_J] = Piece_Logic::mMouseDownRange[i][j];
+								Piece_Logic::mMouseDownRange[i][j] = 258;
+								mENEMY_Right_Cannon_Y += (Move_Y * 60);
+
+								Piece_Logic::__WE__mWE_CommandState(0);//这里重要
+								Piece::Change_mWe(0);
+								ifok = true;
+								break;
+							}
+							else if (Index > 2)
+							{
+								break;
+							}
+							//==========================普通移动
 							if (Piece_Logic::mMouseDownRange[i + (k + 1)][j] >= 200 && Piece_Logic::mMouseDownRange[i + (k + 1)][j] != 999)
 							{
-								IfOkMove = true;
+								if (k == Move_Y - 1)//证明这条路可以走得通
+								{
+									IfOkMove = true;
+								}
 							}
-							else
+							else//不是吃棋或者移动，那还执行个鸡巴
 							{
 								IfOkMove = false;
 								break;
-							}
-							if (IfOkMove == false) //只要是一次false就是不行的
-							{
-								break;
-							}
-							if (true)
-							{
-								//这里可以判断炮可以吃棋了
 							}
 							if (IfOkMove && k == Move_Y - 1)//证明这条路可以走得通
 							{
@@ -184,31 +276,77 @@ public:
 				for (int j = 0; j < 11; j++)
 				{
 					bool IfOkMove = false;
-					if (Piece_Logic::mMouseDownRange[i][j] == 110)//索引到棋子右边的炮
+					if (Piece_Logic::mMouseDownRange[i][j] == 110)//索引到棋子左边的炮
 					{
 						int Temp_X = ENEMY_Command::__mReturnRedCurStandardCoor__().x;
 						int Temp_Y = ENEMY_Command::__mReturnRedCurStandardCoor__().y;//得到鼠标点击的标准坐标
-
 						int Move_X = (mENEMY_Right_Cannon_X - Temp_X) / 60;//得到要移动的次数，同时也是目标的Y坐标
-
+						int Index = 0;//通过这个索引来执行吃棋
+						int Index_I = 0;//记录索引到的第一个棋
+						int Index_J = 0;//记录索引到的第一个棋
+						int Index_I_J = 0;//记录索引到的第一个棋的次数
+						int Index_N = 0;//记录从索引到第一个棋的又开始索引的棋子个数是否违反规则
+						int Index_N_I = 0;//记录索引了几次才索引到被攻击的棋子
+						int Index_Frequency = 0;//保证只执行一次
 						for (int k = 0; k < Move_X; k++)//进行巡路是否可以行走，可以走多少
 						{
+							Index_I_J++;//记录索引到的第一个棋的次数
+							if ((Piece_Logic::mMouseDownRange[i][j - (k + 1)] < 200 &&
+								Piece_Logic::mMouseDownRange[i][j - (k + 1)] != 999))
+							{
+								if (Index_Frequency == 0)//保证只执行一次
+								{
+									Index++;//只要是棋子,不管敌方还是我方，都能作为桥梁
+									Index_I = i;//记录索引到的第一个桥梁
+									Index_J = (j - (k + 1));//记录索引到的第一个桥梁
+									if (Index == 1)//通过这个桥梁找敌方棋子
+									{
+										for (int n = 0; n <= Move_X - Index_I_J - 1; n++)//通过剩下的所有的索引来找这个桥梁找敌方被吃掉棋子的次数
+										{
+											Index_N_I++;
+											if ((Piece_Logic::mMouseDownRange[Index_I][Index_J - (n + 1)] <= 17
+												&& Piece_Logic::mMouseDownRange[Index_I][Index_J - (n + 1)] < 200
+												&& Piece_Logic::mMouseDownRange[Index_I][Index_J - (n + 1)] != 999))
+											{
+												Index_N++;//如果只有一个则可以吃了，如果>1 则违反了规则
+											}
+											if (Index_N == 1 && n == Move_X - Index_I_J - 1)//保证只有一个棋并且是你点击的地方
+											{
+												Index = 2;
+												Index_Frequency = 1;//保证一次索引到位
+												break;
+											}
+										}
+									}
+								}
+							}
+							if (Index == 2)//炮找到了攻击的对象
+							{
+								Piece_Logic::mMouseDownRange[i][j - Move_X] = Piece_Logic::mMouseDownRange[i][j];
+								Piece_Logic::mMouseDownRange[i][j] = 258;
+								mENEMY_Right_Cannon_X -= (Move_X * 60);
+
+								Piece_Logic::__WE__mWE_CommandState(0);//这里重要
+								Piece::Change_mWe(0);
+								ifok = true;
+								break;
+							}
+							else if (Index > 2)
+							{
+								break;
+							}
+							//==========================普通移动
 							if (Piece_Logic::mMouseDownRange[i][j - (k + 1)] >= 200 && Piece_Logic::mMouseDownRange[i][j - (k + 1)] != 999)
 							{
-								IfOkMove = true;
+								if (k == Move_X - 1)//证明这条路可以走得通
+								{
+									IfOkMove = true;
+								}
 							}
-							else
+							else//不是吃棋或者移动，那还执行个鸡巴
 							{
 								IfOkMove = false;
 								break;
-							}
-							if (IfOkMove == false) //只要是一次false就是不行的
-							{
-								break;
-							}
-							if (true)
-							{
-								//这里可以判断炮可以吃棋了
 							}
 							if (IfOkMove && k == Move_X - 1)//证明这条路可以走得通
 							{
@@ -237,31 +375,77 @@ public:
 				for (int j = 0; j < 11; j++)
 				{
 					bool IfOkMove = false;
-					if (Piece_Logic::mMouseDownRange[i][j] == 110)//索引到棋子右边的炮
+					if (Piece_Logic::mMouseDownRange[i][j] == 110)//索引到棋子左边的炮
 					{
 						int Temp_X = ENEMY_Command::__mReturnRedCurStandardCoor__().x;
 						int Temp_Y = ENEMY_Command::__mReturnRedCurStandardCoor__().y;//得到鼠标点击的标准坐标
-
 						int Move_X = (Temp_X - mENEMY_Right_Cannon_X) / 60;//得到要移动的次数，同时也是目标的Y坐标
-
+						int Index = 0;//通过这个索引来执行吃棋
+						int Index_I = 0;//记录索引到的第一个棋
+						int Index_J = 0;//记录索引到的第一个棋
+						int Index_I_J = 0;//记录索引到的第一个棋的次数
+						int Index_N = 0;//记录从索引到第一个棋的又开始索引的棋子个数是否违反规则
+						int Index_N_I = 0;//记录索引了几次才索引到被攻击的棋子
+						int Index_Frequency = 0;//保证只执行一次
 						for (int k = 0; k < Move_X; k++)//进行巡路是否可以行走，可以走多少
 						{
+							Index_I_J++;//记录索引到的第一个棋的次数
+							if ((Piece_Logic::mMouseDownRange[i][j + (k + 1)] < 200 &&
+								Piece_Logic::mMouseDownRange[i][j + (k + 1)] != 999))
+							{
+								if (Index_Frequency == 0)//保证只执行一次
+								{
+									Index++;//只要是棋子,不管敌方还是我方，都能作为桥梁
+									Index_I = i;//记录索引到的第一个桥梁
+									Index_J = (j + (k + 1));//记录索引到的第一个桥梁
+									if (Index == 1)//通过这个桥梁找敌方棋子
+									{
+										for (int n = 0; n <= Move_X - Index_I_J - 1; n++)//通过剩下的所有的索引来找这个桥梁找敌方被吃掉棋子的次数
+										{
+											Index_N_I++;
+											if ((Piece_Logic::mMouseDownRange[Index_I][Index_J + (n + 1)] <= 17
+												&& Piece_Logic::mMouseDownRange[Index_I][Index_J + (n + 1)] < 200
+												&& Piece_Logic::mMouseDownRange[Index_I][Index_J + (n + 1)] != 999))
+											{
+												Index_N++;//如果只有一个则可以吃了，如果>1 则违反了规则
+											}
+											if (Index_N == 1 && n == Move_X - Index_I_J - 1)//保证只有一个棋并且是你点击的地方
+											{
+												Index = 2;
+												Index_Frequency = 1;//保证一次索引到位
+												break;
+											}
+										}
+									}
+								}
+							}
+							if (Index == 2)//炮找到了攻击的对象
+							{
+								Piece_Logic::mMouseDownRange[i][j + Move_X] = Piece_Logic::mMouseDownRange[i][j];
+								Piece_Logic::mMouseDownRange[i][j] = 258;
+								mENEMY_Right_Cannon_X += (Move_X * 60);
+
+								Piece_Logic::__WE__mWE_CommandState(0);//这里重要
+								Piece::Change_mWe(0);
+								ifok = true;
+								break;
+							}
+							else if (Index > 2)
+							{
+								break;
+							}
+							//==========================普通移动
 							if (Piece_Logic::mMouseDownRange[i][j + (k + 1)] >= 200 && Piece_Logic::mMouseDownRange[i][j + (k + 1)] != 999)
 							{
-								IfOkMove = true;
+								if (k == Move_X - 1)//证明这条路可以走得通
+								{
+									IfOkMove = true;
+								}
 							}
-							else
+							else//不是吃棋或者移动，那还执行个鸡巴
 							{
 								IfOkMove = false;
 								break;
-							}
-							if (IfOkMove == false) //只要是一次false就是不行的
-							{
-								break;
-							}
-							if (true)
-							{
-								//这里可以判断炮可以吃棋了
 							}
 							if (IfOkMove && k == Move_X - 1)//证明这条路可以走得通
 							{
